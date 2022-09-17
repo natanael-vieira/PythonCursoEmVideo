6a3b19155edd2b4e968fc5f6ff1e586af272ba47
Natanael Vieira <natanaelsales@live.com>
2022-09-17 10:06:06 -0300

    Create teste01.py

---
+++ ScriptsExercícios/teste01.py
@@ -0,0 +1,5 @@
+nome = input('Qual é seu nome? \n')
+idade = input('Qual é sua idade? \n')
+peso = input('Qual é seu peso? \n')
+
+print ('Seu nome é', nome, ', sua idade é', idade, 'anos', ' e seu peso é', peso, 'Kg.') 
