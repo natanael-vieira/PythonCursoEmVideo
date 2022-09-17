ec9cf1ad2a5abcbd7b051951f7b749423d0b9211
Natanael Vieira <natanaelsales@live.com>
2022-09-17 11:04:18 -0300

    Update ex002.py

--- Exercicios/ex002.py
+++ Exercicios/ex002.py
@@ -1,2 +1,4 @@
+# Eu posso realizar a 'formatação' do print usando o '.format()', neste caso eu posso escrever o print completo e onde eu quero que o programa chame o dado que foi capturado pelo input eu abro e fecho chaves no local '{}' e no final da String, quando eu fechar as aspas eu coloco '.format(variável)' desse modo o nome caputador pelo input vai ser inserido no local que eu defini as chaves.
+
 nome = input('Por favor, digite seu nome: \n')
-print ('Olá', nome,'seja muito bem vindo(a) ao curso de Python do Curso em Vídeo!')
+print ('Olá {} seja muito bem vindo(a) ao curso de Python do Curso em Vídeo!'.format(nome))
