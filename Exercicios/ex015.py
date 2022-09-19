911dd52594534ed78ea804a603f1f35659dfeec8
Natanael Vieira <natanaelsales@live.com>
2022-09-19 07:44:20 -0300

    Update ex015.py

--- Exercicios/ex015.py
+++ Exercicios/ex015.py
@@ -2,4 +2,4 @@ dias = int(input('Por quantos dias o carro foi alugado? '))
 km = float(input('Quantos quilômentros (km) o carro rodou? '))
 pagar = (dias * 60) + (km * 0.15)
 
-print(f'O carro foi alugado por {dias} dias e rodou {km:.2f}km. Portanto o valor final do aluguel é de R${pagar:.2f}')
+print(f'O carro foi alugado por {dias} dias e rodou {km:.1f}km. Portanto o valor final do aluguel é de R${pagar:.2f}')
