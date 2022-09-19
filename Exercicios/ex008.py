952ef28acc88fd6341429732a445d37992890ecc
Natanael Vieira <natanaelsales@live.com>
2022-09-19 06:41:39 -0300

    Update ex008.py

--- Exercicios/ex008.py
+++ Exercicios/ex008.py
@@ -1,5 +1,8 @@
 metro = float(input('Digite a metragem: '))
 cm = metro * 100
 mm = metro * 1000
+km = metro / 1000
+dam = metro / 100
+dm = metro * 10 
 
-print(f'A metragem {metro} tem {cm}cm e {mm}mm.')
+print(f'A medida {metro}m tem: \n{km}km \n{dam}dam \n{dm}dm \n{cm}cm \n{mm}mm.')
