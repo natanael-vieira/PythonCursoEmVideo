fb2ce85e9edf86d264ab6c20d7d47c8f2f44e9b2
Natanael Vieira <natanaelsales@live.com>
2022-09-19 05:50:57 -0300

    Create ex005.py

---
+++ Exercicios/ex005.py
@@ -0,0 +1,58 @@
+n1 = int(input('Digite um valor:\n'))
+n2 = int(input('Digite outro valor:\n'))
+
+s = n1 + n2
+sub = n1 - n2
+m = n1 * n2
+d = n1 / n2
+rd = n1 // n2
+e = n1 ** n2
+mod = s%2
+
+print(f'A soma dos dois números é {s}. A subtração é {sub}. A multiplicação é {m}!')
+print(f'A divisão dos dois números é {d}. Já divisão inteira é {rd}. A exponenciação é {e}.')
+print(f'O módulo da soma por 2 é {mod}')
+
+print('='*100)
+
+num1 = int((input('Digite um número qualquer: ')))
+ant = num1 - 1
+suc = num1 + 1
+dob = num1 * 2
+tri = num1 * 3
+r2 = num1 ** (1/2)
+
+print(f'O antecessor de {num1} é {ant} e seu sucessor é {suc}')
+
+print(f'O dobro de {num1} é {dob}, seu triplo é {tri} e sua raiz quadrada é {r2}')
+
+print('='*100)
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+
+print(f'A média das notas é: {media}')
+
+print('='*100)
+
+metro = float(input('Digite a metragem: '))
+cm = metro * 100
+mm = metro * 1000
+
+print(f'A metragem {metro} tem {cm}cm e {mm}mm.')
+
+print('='*100)
+
+nume1 = int(input('Digite um número inteiro qualquer: '))
+tab1 = nume1 * 1
+tab2 = nume1 * 2
+tab3 = nume1 * 3
+tab4 = nume1 * 4
+tab5 = nume1 * 5
+tab6 = nume1 * 6
+tab7 = nume1 * 7
+tab8 = nume1 * 8
+tab9 = nume1 * 9
+tab10 = nume1 * 10
+print(f'A tabuada de {nume1} é: \n {tab1}\n {tab2}\n {tab3}\n {tab4}\n {tab5}\n {tab6}\n {tab7}\n {tab8}\n {tab9}\n {tab10}\n')
