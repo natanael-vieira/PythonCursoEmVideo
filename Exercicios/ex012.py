ed3444d26a7a49e5b3f72561ffe23bfc9004dccc
Natanael Vieira <natanaelsales@live.com>
2022-09-19 06:51:33 -0300

    Update ex012.py

--- Exercicios/ex012.py
+++ Exercicios/ex012.py
@@ -1,4 +1,4 @@
-preco = float(input('Digite o preço do produto: '))
+preco = float(input('Digite o preço do produto: R$ '))
 desc = preco - (preco * (5/100))
 
 print(f'O preço do produto é de R${preco:.2f}, mas com o desconto de 5% fica R${desc:.2f}')
