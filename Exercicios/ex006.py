bfdeb395a4429e68c731325e7a9333f6faff908f
Natanael Vieira <natanaelsales@live.com>
2022-09-19 05:52:22 -0300

    Create ex006.py

---
+++ Exercicios/ex006.py
@@ -0,0 +1,6 @@
+num1 = int((input('Digite um número qualquer: ')))
+dob = num1 * 2
+tri = num1 * 3
+r2 = num1 ** (1/2)
+
+print(f'O dobro de {num1} é {dob}, seu triplo é {tri} e sua raiz quadrada é {r2}')
