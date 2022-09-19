38f4cc6c55d2e9aeed97763fb960223b9af171cf
Natanael Vieira <natanaelsales@live.com>
2022-09-19 06:16:55 -0300

    Update ex005.py

--- Exercicios/ex005.py
+++ Exercicios/ex005.py
@@ -18,41 +18,5 @@ print('='*100)
 num1 = int((input('Digite um número qualquer: ')))
 ant = num1 - 1
 suc = num1 + 1
-dob = num1 * 2
-tri = num1 * 3
-r2 = num1 ** (1/2)
 
 print(f'O antecessor de {num1} é {ant} e seu sucessor é {suc}')
-
-print(f'O dobro de {num1} é {dob}, seu triplo é {tri} e sua raiz quadrada é {r2}')
-
-print('='*100)
-
-nota1 = float(input('Digite a primeira nota: '))
-nota2 = float(input('Digite a segunda nota: '))
-media = (nota1 + nota2) / 2
-
-print(f'A média das notas é: {media}')
-
-print('='*100)
-
-metro = float(input('Digite a metragem: '))
-cm = metro * 100
-mm = metro * 1000
-
-print(f'A metragem {metro} tem {cm}cm e {mm}mm.')
-
-print('='*100)
-
-nume1 = int(input('Digite um número inteiro qualquer: '))
-tab1 = nume1 * 1
-tab2 = nume1 * 2
-tab3 = nume1 * 3
-tab4 = nume1 * 4
-tab5 = nume1 * 5
-tab6 = nume1 * 6
-tab7 = nume1 * 7
-tab8 = nume1 * 8
-tab9 = nume1 * 9
-tab10 = nume1 * 10
-print(f'A tabuada de {nume1} é: \n {tab1}\n {tab2}\n {tab3}\n {tab4}\n {tab5}\n {tab6}\n {tab7}\n {tab8}\n {tab9}\n {tab10}\n')
