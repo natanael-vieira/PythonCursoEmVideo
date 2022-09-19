d6809995a843ee1416ce40e0e3737a91b4496d87
Natanael Vieira <natanaelsales@live.com>
2022-09-19 05:54:04 -0300

    Create ex009.py

---
+++ Exercicios/ex009.py
@@ -0,0 +1,12 @@
+nume1 = int(input('Digite um número inteiro qualquer: '))
+tab1 = nume1 * 1
+tab2 = nume1 * 2
+tab3 = nume1 * 3
+tab4 = nume1 * 4
+tab5 = nume1 * 5
+tab6 = nume1 * 6
+tab7 = nume1 * 7
+tab8 = nume1 * 8
+tab9 = nume1 * 9
+tab10 = nume1 * 10
+print(f'A tabuada de {nume1} é: \n {tab1}\n {tab2}\n {tab3}\n {tab4}\n {tab5}\n {tab6}\n {tab7}\n {tab8}\n {tab9}\n {tab10}\n')
