827187e651c7fcb87072b63fa248f5ae1fc037bb
Natanael Vieira <natanaelsales@live.com>
2022-09-19 06:15:58 -0300

    Create ex013.py

---
+++ Exercicios/ex013.py
@@ -0,0 +1,4 @@
+sal = float(input('Digite o salário do funcionário: '))
+nsal = sal + (sal *(15/100))
+
+print(f'O salário atual do funcionário é de R${sal:.2f} e com o acréscimo de 15% passa a ser de R${nsal:.2f}')
