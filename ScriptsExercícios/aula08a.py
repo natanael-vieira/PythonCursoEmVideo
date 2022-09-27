c5a6bd5eaa1585cbbe5d133457c78cebd9fb6a1f
Natanael Vieira <natanaelsales@live.com>
2022-09-27 06:23:32 -0300

    Update aula08a.py

--- ScriptsExercícios/aula08a.py
+++ ScriptsExercícios/aula08a.py
@@ -1,5 +1,15 @@
 import math
+import random
+import emoji
+
 num = int(input('Digite um número: '))
 raiz = math.sqrt(num)
 
 print(f'A raiz de {num} é igual a {raiz:.2f}')
+
+n = random.randint(0, 400)
+square = math.sqrt(n)
+
+print(f'A raiz de {n} é {square:.3f}.')
+
+print(emoji.emojize('E é assim que se faz raiz quadrada em Python :sunglasses: !'))
