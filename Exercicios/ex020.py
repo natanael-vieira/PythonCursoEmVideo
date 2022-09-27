59e5c89c2b215a200cfb5ecd3b3398b4510a6272
Natanael Vieira <natanaelsales@live.com>
2022-09-27 07:31:04 -0300

    Create ex020.py

---
+++ Exercicios/ex020.py
@@ -0,0 +1,12 @@
+import random
+
+alu1 = str(input('Digite o primeiro nome: '))
+alu2 = str(input('Digite o segundo nome: '))
+alu3 = str(input('Digite o terceiro nome: '))
+alu4 = str(input('Digite o quarto nome: '))
+
+lista = [alu1, alu2, alu3, alu4]
+
+random.shuffle(lista)
+
+print(f'A sequência da apresentação será: {lista}.')
