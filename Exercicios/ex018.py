bfb4d2d0be83275b297ee196aa7e397a22116b84
Natanael Vieira <natanaelsales@live.com>
2022-09-27 07:20:41 -0300

    Update ex018.py

--- Exercicios/ex018.py
+++ Exercicios/ex018.py
@@ -2,8 +2,8 @@ import math
 
 ang = int(input('Digite o valor do ângulo (entre 0º e 360º): '))
 
-sen = math.sin(ang)
-cos = math.cos(ang)
-tg = math.tan(ang)
+sen = math.sin(math.radians(ang))
+cos = math.cos(math.radians(ang))
+tg = math.tan(math.radians(ang))
 
 print(f'O ângulo de {ang}º tem o seno {sen:.2f}, o cosseno {cos:.2f} e a tangente {tg:.2f}.')
