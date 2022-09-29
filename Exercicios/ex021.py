8481ae3bca447929d2baf8b0171ea55dfa9a6b21
Natanael Vieira <natanaelsales@live.com>
2022-09-29 20:59:53 -0300

    Update ex021.py

--- Exercicios/ex021.py
+++ Exercicios/ex021.py
@@ -3,4 +3,5 @@ import pygame
 pygame.init()
 pygame.mixer.music.load('ex021.mp3')
 pygame.mixer.music.play()
+# permite que eu consiga executar a música, quanto maior o número, maior o tempo de reprodução.
 pygame.time.delay(200000000)
