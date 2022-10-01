a725bee0e64ce5b325e01c181238117c0c219d22
Natanael Vieira <natanaelsales@live.com>
2022-10-01 09:11:32 -0300

    Create aula10a.py

---
+++ ScriptsExercícios/aula10a.py
@@ -0,0 +1,14 @@
+time = int(input('Quantos anos tem seu carro? '))
+
+if time <= 5:
+    print('Seu carro é novo!')
+else:
+    print('Seu carro é clássico!')
+
+print('=== FIM ===')
+
+# Ou simplificando o código fica dessa forma
+
+# tempo = int(input("Quantos anos tem seu carro? "))
+# print('Seu carro é novo!' if tempo <= 5 else 'Seu carro é clássico')
+# print('=== É ISSO ===')
