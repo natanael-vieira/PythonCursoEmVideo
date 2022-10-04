432683c304d99982726df9be3c9f3c8fad9c4083
Natanael Vieira <natanaelsales@live.com>
2022-10-04 06:58:36 -0300

    Create aula12a.py

---
+++ ScriptsExercícios/aula12a.py
@@ -0,0 +1,10 @@
+name = str(input('Qual seu nome? '))
+
+if name == 'Thais':
+    print('Mamãe mais linda de todas!')
+elif name == 'Abigail' or name == 'Rute' or name == 'Timóteo':
+    print('Meu bebezinho lindo ^^')
+else:
+    print('Vai estudar')
+
+print(f'Tenha um bom dia, {name}!')
