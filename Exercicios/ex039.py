9e874101853d1f70928fa211709f989eba6d1777
Natanael Vieira <natanaelsales@live.com>
2022-10-04 09:42:30 -0300

    Update ex039.py

--- Exercicios/ex039.py
+++ Exercicios/ex039.py
@@ -1,4 +1,5 @@
 import datetime
+from tkinter.filedialog import SaveFileDialog
 
 print('===== SERVIÇO MILITAR =====\n')
 
@@ -9,9 +10,12 @@ date = currentDateTime.date()
 year = date.strftime("%Y")
 idade = int(year) - ano
 
+prazo = idade - 18
+
 if idade == 18:
-    print(f'\nEsse é seu ano de se alistar, pois possui ou vai completar {idade} anos.')
+    print(f'\nEsse é seu ano de se alistar, pois possui ou vai completar {idade} anos este ano.')
 elif idade > 18:
-    print('\nVocê está em débito com o serviço militar, já passou da idade de se alistar. Compareça a uma unidade militar para se regularizar.')
+    print(f'\nVocê está em débito com o serviço militar, já se passou {prazo} anos do alistamento. Compareça a uma unidade militar para se regularizar.')
 else:
-    print('\nVocê ainda não está na idade de se alistar!')
+    saldo = 18 - idade
+    print(f'\nVocê ainda não está na idade de se alistar, falta(m) {saldo} ano(s) para o alistamento!')
