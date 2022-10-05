4f91e9c1fe273f8a4ae122d7da4a6cc36693aa8d
Natanael Vieira <natanaelsales@live.com>
2022-10-05 04:59:26 -0300

    Update ex037.py

--- Exercicios/ex037.py
+++ Exercicios/ex037.py
@@ -1,16 +1,16 @@
 print('===== TRANSFORMAR NÚMEROS =====\n')
 
 num = int(input('Digite um número qualquer: '))
-esc = int(input('Escolha um número entre 1, 2 ou 3: '))
+esc = int(input('Escolha um número entre: \n1 - BINÁRIO \n2 - OCTAL \n3 - HEXADECIMAL: '))
 
 if esc == 1:
     bi = bin(num)
-    print(bi)
+    print(f'O número {num} em BINÁRIO é {bi[2:]}')
 elif esc == 2:
     oc = oct(num)
-    print(oc)
+    print(f'O número {num} em OCTAL é {oc[2:]}')
 elif esc == 3:
     he = hex(num)
-    print(he)
+    print(f'O número {num} em HEXADECIMAL é {he[2:]}')
 else:
     print('Digite uma opção válida!')
