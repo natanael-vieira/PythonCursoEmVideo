d59fb40b18b7ad443ab1e8d0ab439428bbb2c3b3
Natanael Vieira <natanaelsales@live.com>
2022-10-06 06:31:07 -0300

    Create aula13a.py

---
+++ ScriptsExercícios/aula13a.py
@@ -0,0 +1,23 @@
+# n = int(input('Digite um número: '))
+
+# for i in range(0, n+1):
+#     print(i)
+# print('\nFim!')
+
+# i = int(input('Digite o início da contagem: '))
+# f = int(input('\nDigite o fim da contagem: '))
+# p = int(input('\nDigite o passo da contagem: '))
+
+# for c in range(i, f+1, p):
+#     print(c)
+
+# for c in range(0, 5):
+#     n = int(input('\nDigite um número: '))
+# print('\nFIM\n')
+
+s = 0
+for c in range(0 , 3):
+    n = int(input('Digite um número: '))
+    s += n
+
+print(f'A soma de todos os números é: {s}')
