e09055c8f53e0f264ef1cedcd69b2d1667c4e397
Natanael Vieira <natanaelsales@live.com>
2022-10-08 11:19:53 -0300

    Update ex066.py

--- Exercicios/ex066.py
+++ Exercicios/ex066.py
@@ -1,10 +1,10 @@
 n = s = c = 0
 
 while True:
-    n = int(input('Digite um número: '))
+    n = int(input('Digite um número [999 PARAR]: '))
     if n == 999:
         break
     s += n
     c += 1
 
-print(f'\nA quantidade de números digitados foi {c} e a SOMA foi {s}\n')
+print(f'\nA quantidade de números digitados foi {c} e a SOMA deles foi {s}.\n')
