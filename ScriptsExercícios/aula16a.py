6f204367599ab225a02552216c8ff2abc74c0fb6
Natanael Vieira <natanaelsales@live.com>
2022-10-11 07:14:56 -0300

    Create aula16a.py

---
+++ ScriptsExercícios/aula16a.py
@@ -0,0 +1,11 @@
+rango = ('Arroz', 'Feijão', 'Bife', 'Batata Frita', 'Refrigerante')
+
+for cont in range(0, len(rango)):
+    print(f'Eu vou comer no almoço: {rango[cont]}')
+
+print('\nO almoço estava demais!\n')
+
+for c in rango:
+    print(f'Eu vou comer no almoço: {c}')
+
+print('\nO almoço estava demais!')
