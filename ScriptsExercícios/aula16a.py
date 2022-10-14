30188724352cd6160da16e238f05d78c87341679
Natanael Vieira <natanaelsales@live.com>
2022-10-14 06:28:50 -0300

    Update aula16a.py

--- ScriptsExercícios/aula16a.py
+++ ScriptsExercícios/aula16a.py
@@ -1,7 +1,7 @@
 rango = ('Arroz', 'Feijão', 'Bife', 'Batata Frita', 'Refrigerante')
 
 for cont in range(0, len(rango)):
-    print(f'Eu vou comer no almoço: {rango[cont]}')
+    print(f'Eu vou comer no almoço: {rango[cont]} na posição {cont}')
 
 print('\nO almoço estava demais!\n')
 
@@ -9,3 +9,10 @@ for c in rango:
     print(f'Eu vou comer no almoço: {c}')
 
 print('\nO almoço estava demais!')
+
+for pos, comida in enumerate (rango):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+print('\nO almoço estava demais!\n')
+
+print(sorted(rango)) #Aqui o sorted vai ordenar a tupla em ordem alfabética/numérica, além de criar uma lista com ela
