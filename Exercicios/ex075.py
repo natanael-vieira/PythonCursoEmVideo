65fe340bc80a0d0bf915abb72ddcce574b5ab675
Natanael Vieira <natanaelsales@live.com>
2022-10-14 09:23:42 -0300

    Create ex075.py

---
+++ Exercicios/ex075.py
@@ -0,0 +1,18 @@
+print('===== USANDO TUPLAS E NÚMEROS =====\n')
+
+num = (int(input('Digite um número: ')), int(input('Digite um número: ')), int(input('Digite um número: ')), int(input('Digite um número: ')))
+
+print(f'Os número digitados foram: {num}')
+
+print(f'O número 9 apareceu {num.count(9)} vezes')
+
+if 3 in num:
+    print(f'O número 3 apareceu na {num.index(3)+1}ª posição!')
+else:
+    print('Não foi digitado nenhum número 3!')
+
+print('Os valores pares digitados foram: ', end = '')
+
+for n in num:
+    if n % 2 == 0:
+        print(n, end = ' e ')
