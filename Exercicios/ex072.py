9d6bfb6a5fc9ca29e6a7e60b1d3a3c390802018d
Natanael Vieira <natanaelsales@live.com>
2022-10-14 06:56:09 -0300

    Create ex072.py

---
+++ Exercicios/ex072.py
@@ -0,0 +1,71 @@
+print('===== VER NÚMERO POR EXTENSO =====\n')
+ext = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quize', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+dig = int(input('Digite um número entre 0 e 20: '))
+
+while True:
+    if dig == 0:
+        print(f'Você digitou o número {ext[0]}!')
+        break
+    elif dig == 1:
+        print(f'Você digitou o número {ext[1]}')
+        break
+    elif dig == 2:
+        print(f'Você digitou o número {ext[2]}')
+        break
+    elif dig == 3:
+        print(f'Você digitou o número {ext[3]}')
+        break
+    elif dig == 4:
+        print(f'Você digitou o número {ext[4]}')
+        break
+    elif dig == 5:
+        print(f'Você digitou o número {ext[5]}')
+        break
+    elif dig == 6:
+        print(f'Você digitou o número {ext[6]}')
+        break
+    elif dig == 7:
+        print(f'Você digitou o número {ext[7]}')
+        break
+    elif dig == 8:
+        print(f'Você digitou o número {ext[8]}')
+        break
+    elif dig == 9:
+        print(f'Você digitou o número {ext[9]}')
+        break
+    elif dig == 10:
+        print(f'Você digitou o número {ext[10]}')
+        break
+    elif dig == 11:
+        print(f'Você digitou o número {ext[11]}')
+        break
+    elif dig == 12:
+        print(f'Você digitou o número {ext[12]}')
+        break
+    elif dig == 13:
+        print(f'Você digitou o número {ext[13]}')
+        break
+    elif dig == 14:
+        print(f'Você digitou o número {ext[14]}')
+        break
+    elif dig == 15:
+        print(f'Você digitou o número {ext[15]}')
+        break
+    elif dig == 16:
+        print(f'Você digitou o número {ext[16]}')
+        break
+    elif dig == 17:
+        print(f'Você digitou o número {ext[17]}')
+        break
+    elif dig == 18:
+        print(f'Você digitou o número {ext[18]}')
+        break
+    elif dig == 19:
+        print(f'Você digitou o número {ext[19]}')
+        break
+    elif dig == 20:
+        print(f'Você digitou o número {ext[20]}')
+        break
+    else: 
+        dig = int(input('TENTE NOVAMENTE! Digite um número entre 0 e 20: '))
+
