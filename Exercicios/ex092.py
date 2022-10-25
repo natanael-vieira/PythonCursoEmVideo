877ba73099ebe85e7a358b64cef6d2d5f291ea96
Natanael Vieira <natanaelsales@live.com>
2022-10-25 09:41:19 -0300

    Create ex092.py

---
+++ Exercicios/ex092.py
@@ -0,0 +1,35 @@
+from datetime import date
+
+print('===== CADASTRO DE TRABALHADORES E APOSENTADORIA =====\n')
+
+prev = {}
+
+prev['nome'] = str(input('Digite o nome do trabalhador: ')).capitalize()
+ano = int(input('Digite o ano de nascimento: '))
+
+idade = 2022 - ano
+prev['nascimento'] = ano
+prev['idade'] = idade
+
+prev['ctps'] = int(input('Digite o nº da CTPS: [0 - NÃO POSSUI] '))
+if prev['ctps'] == 0:
+    print(prev)
+    print()
+    for k, v in prev.items():
+        print(f'{k} tem o valor {v}')
+else:
+    contr = int(input('Digite o ano de contratação: '))
+    anosTrab = 2022 - contr
+    apos = 35 - anosTrab
+    prev['contratação'] = contr
+    prev['aposenta'] = apos + idade
+
+    prev['salario'] = float(input('Digite o salário: '))
+
+    print(prev)
+    print()
+
+    for k, v in prev.items():
+        print(f'{k} tem o valor {v}')
+
+
