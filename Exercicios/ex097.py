0af2202698915084a190e4d10f476fbcc478eaf8
Natanael Vieira <natanaelsales@live.com>
2022-10-28 05:38:00 -0300

    Create ex097.py

---
+++ Exercicios/ex097.py
@@ -0,0 +1,12 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+#Programa Principal
+escreva('Natanael Vieira')
+escreva('Thais Sales Figueira Oliveira Vieira')
+escreva('Abigail Sales Figueira Oliveira Vieira')
+escreva('Rute Sales Figueira Oliveira Vieira')
