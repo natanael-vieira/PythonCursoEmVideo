c670c005d935852d4eac468645836119de188573
Natanael Vieira <natanaelsales@live.com>
2022-10-29 15:36:55 -0300

    Create ex101.py

---
+++ Exercicios/ex101.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+print('===== PRECISO VOTAR? =====\n')
+
+def votar(ano):
+    idade = datetime.now().year - ano
+ 
+    if idade < 16:
+        return f'Com {idade} anos de idade NÃO VOTA!'
+    elif idade >= 16 and idade < 18:
+        return f'Com {idade} anos de idade o VOTO É FACULTATIVO!'
+    elif idade >= 18 and idade < 65:
+        return f'Com {idade} anos de idade o VOTO É OBRIGATÓRIO!'
+    elif idade >= 65:
+        return f'Com {idade} anos de idade o VOTO É FACULTATIVO!'
+
+
+#Programa Principal
+
+ano = int(input('Digite seu ano de nascimento: '))
+votar(ano)
+print(votar(ano))
