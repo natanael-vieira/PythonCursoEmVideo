dea1fd294ef54ccf5fbb5de0d64774ac14d9349f
Natanael Vieira <natanaelsales@live.com>
2022-11-08 18:24:36 -0300

    Update ex108.py

--- Exercicios/ex108/ex108.py
+++ Exercicios/ex108/ex108.py
@@ -4,5 +4,5 @@ preco = float(input('Digite o preço do produto: R$ '))
 
 print(f'\nA METADE de {moeda.moeda(preco)} é {moeda.moeda(moeda.metade(preco))}')
 print(f'O DOBRO do {moeda.moeda(preco)} é {moeda.moeda(moeda.dobro(preco))}')
-print(f'O preço {moeda.moeda(preco)} com ACRÉCIMO de 17% é {moeda.moeda(moeda.aumentar(preco))}')
-print(f'O preço {moeda.moeda(preco)} com DESCONTO de 11% é {moeda.moeda(moeda.diminuir(preco))}')
+print(f'Com ACRÉCIMO de 17% fica {moeda.moeda(moeda.aumentar(preco))}')
+print(f'Com DESCONTO de 11% fica {moeda.moeda(moeda.diminuir(preco))}')
