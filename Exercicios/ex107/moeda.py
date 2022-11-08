2bcd2b35168acc3b0e15e8d1437b7c95296ba4be
Natanael Vieira <natanaelsales@live.com>
2022-11-08 04:54:29 -0300

    Create moeda.py

---
+++ Exercicios/ex107/moeda.py
@@ -0,0 +1,16 @@
+def aumentar(n):
+    n = n + ((n * 17) / 100)
+    return n
+
+
+def diminuir(n):
+    n = n - ((n * 11) / 100)
+    return n
+
+
+def dobro(n):
+    return n * 2
+
+
+def metade(n):
+    return n / 2
