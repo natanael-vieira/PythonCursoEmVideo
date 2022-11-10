f349724c5b8261a93784fa4a710178a3f71535b8
Natanael Vieira <natanaelsales@live.com>
2022-11-10 19:38:33 -0300

    Update ex115.py

--- Exercicios/ex115/ex115.py
+++ Exercicios/ex115/ex115.py
@@ -1,19 +1,30 @@
-from lib import interface
+from lib.interface import *
+from lib.arquivo import *
 from time import sleep
 
 
+arq = 'teste.txt'
+
+
+if arquivoExiste(arq):
+    print('Arquivo EXISTENTE!')
+else:
+    print('Arquivo INEXISTENTE!')
+    criarArquivo(arq)
+
+
 while True:
-    resp = interface.menu(['Ver Pessoas Cadastradas', 'Cadastrar Nova Pessoa', 'SAIR do Sistema'])
+    resp = menu(['Ver Pessoas Cadastradas', 'Cadastrar Nova Pessoa', 'SAIR do Sistema'])
 
     if resp == 1:
-        interface.cab('PESSOAS CADASTRADAS')
+        lerArquivo(arq)
     elif resp == 2:
-        interface.cab('CADASTRAR NOVA PESSOA')
+        cab('CADASTRAR NOVA PESSOA')
     elif resp == 3:
         sleep(1)
-        interface.cab('Saindo do Sistema. Até logo!')
+        cab('Saindo do Sistema. Até logo!')
         sleep(1)
         break
     else:
-        interface.cab('ERRO! Digite uma opção válida!')
+        cab('ERRO! Digite uma opção válida!')
     sleep(1)
